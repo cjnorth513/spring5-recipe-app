cf4383b5f739866c16106559ff0d4f4850f4d78d
cjnorth_513 <cjnorthington@gmail.com>
2022-03-20 16:49:10 -0400

    Created Spring Data JPA data methods

--- src/main/java/guru/springframework/controllers/indexController.java
+++ src/main/java/guru/springframework/controllers/indexController.java
@@ -1,12 +1,17 @@
 package guru.springframework.controllers;
 
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class indexController {
+    public indexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
 
-   @RequestMapping({"", "/", "/index"})
+    }
+
+    @RequestMapping({"", "/", "/index"})
    public String getIndexPage(){
        return "index";
    }
